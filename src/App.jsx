701800656ac7ae9de4828ff22067516d258eae49
Ryan Dalton <rdalton2405@gmail.com>
2026-06-27 14:32:47 -0400

    testing react

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -115,7 +118,9 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+      <Footer />
     </>
+    
   )
 }
 
