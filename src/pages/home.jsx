2baefb7632032ca7e28b769d76e494bd065cc5ad
Ryan Dalton <rdalton2405@gmail.com>
2026-07-26 19:44:39 -0400

    test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@
         <>
             <h1>This is the home page</h1>
             <p>it dont look like it but believe me im working on it</p>
+            <p>testing automatic deployment. please bear with me in these trying times.</p>
         </>
     )
 }
