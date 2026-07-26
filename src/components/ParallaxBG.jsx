08a50bf2ce1fbab5772634474612ae3e5b06e141
Ryan Dalton <rdalton2405@gmail.com>
2026-07-26 15:21:42 -0400

    textures for wip bg

---
+++ src/components/ParallaxBG.jsx
@@ -0,0 +1,11 @@
+// react + webgl  imma figure it out
+// scrolling windwaker waves texture + a red dolphin fin that swims by occasionally 
+
+function WindWakerWater(){
+    return (
+        <>
+        </>
+    )
+}
+
+export default WindWakerWater
