105487bceb5b95601e7750c4763e4ab222078bc5
Ryan Dalton <rdalton2405@gmail.com>
2026-07-26 23:09:51 -0400

    typo lol

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,7 @@
 function Contact() {
     return (
         <>
-            <h1>Here's how to get ahold of me</h1>
+            <h1>Here's how to get a hold of me</h1>
             <a href="mailto:rdalton2405@gmail.com">Gmail: rdalton2405@gmail.com</a>
             <a href="https://github.com/rydal05">GitHub</a>
 
