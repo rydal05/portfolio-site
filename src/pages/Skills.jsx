5936c9f408a85e37c781832a39bb0865412d23f6
Ryan Dalton <rdalton2405@gmail.com>
2026-07-31 12:40:07 -0400

    updated skills to use dynamic list

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,54 +1,60 @@
+import ListModal from "../components/ListModal";
+
+const frameworks = [
+	{ name: "React", icon: "" },
+	{ name: "Flask", icon: "/about" },
+	{ name: "Svelte", icon: "/portfolio" },
+	{ name: "SQLite", icon: "/contact" },
+	{ name: "BeautifulSoup", icon: "/skills" },
+	{ name: "TailWindCSS", icon: "/skills" },
+];
+
+const AIML = [
+	{ name: "Hugging Face", icon: "/skills" },
+	{ name: "Ollama", icon: "/skills" },
+	{ name: "Continue Coding Agent", icon: "/skills" },
+];
+
+const tools = [
+	{ name: "Docker", icon: "/skills" },
+	{ name: "Linux", icon: "/skills" },
+	{ name: "Ghidra", icon: "/skills" },
+	{ name: "Ida", icon: "/skills" },
+	{ name: "Git", icon: "/skills" },
+];
+
+const languages = [
+	{ name: "C", icon: "/skills" },
+	{ name: "C++", icon: "/skills" },
+	{ name: "Python", icon: "/skills" },
+	{ name: "SQL", icon: "/skills" },
+	{ name: "Java", icon: "/skills" },
+	{ name: "HTML5", icon: "/skills" },
+	{ name: "CSS", icon: "/skills" },
+	{ name: "JavaScript", icon: "/skills" },
+	{ name: "Lua", icon: "/skills" },
+	{ name: "Delphi", icon: "/skills" },
+	{ name: "GDScript", icon: "/skills" },
+	{ name: "Rust", icon: "/skills" },
+];
+
 function Skills() {
 	return (
 		<>
 			<div>
-				<h1>Frameworks & Libraries</h1>
-				<ul>
-					<li>React</li>
-					<li>Flask</li>
-					<li>Svelte</li>
-					<li>SQLite</li>
-					<li>BeautifulSoup</li>
-					<li>Tailwind</li>
-				</ul>
+				<ListModal title="Frameworks & Libraries" text={frameworks} />
 			</div>
 
 			<div>
-				<h1>Languages</h1>
-				<ul>
-					<li>C</li>
-					<li>C++</li>
-					<li>Python</li>
-					<li>SQL</li>
-					<li>Java</li>
-					<li>HTML5</li>
-					<li>CSS</li>
-					<li>JavaScript</li>
-					<li>Lua</li>
-					<li>Delphi</li>
-					<li>GDScript</li>
-					<li>Rust</li>
-				</ul>
+				<ListModal title="Languages" text={languages} />
 			</div>
 
 			<div>
-				<h1>AI & ML</h1>
-				<ul>
-					<li>Hugging Face</li>
-					<li>Ollama</li>
-					<li>Continue Coding Agent</li>
-				</ul>
+				<ListModal title="AI & Machine Learning" text={AIML} />
 			</div>
 
 			<div>
-				<h1>Tools</h1>
-				<ul>
-					<li>Docker</li>
-					<li>Linux</li>
-					<li>Ghidra</li>
-					<li>Ida</li>
-					<li>Git</li>
-				</ul>
+				<ListModal title="Tools" text={tools} />
 			</div>
 		</>
 	);
