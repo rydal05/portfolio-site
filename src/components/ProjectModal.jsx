2c124dc52734aa551efb68f23c93c2c7cea87c7b
Ryan Dalton <rdalton2405@gmail.com>
2026-07-31 11:56:12 -0400

    modal style update

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,11 +1,17 @@
-function Project({link, title, image, text} ){
-    return (
-        <>
-            <h2><a href={link}>{title}</a></h2>
-            <img src={image}></img>
-            <p>text</p>
-        </>
-    )
+function ProjectModal({ link, title, image, text }) {
+  return (
+    <>
+      <div class="flex-1 flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
+        <h2>
+          <a class="hover:text-[#1d7cff] text-[#e94f37]" href={link}>
+            {title}
+          </a>
+        </h2>
+        <img src={image}></img>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
 
-export default Project
+export default ProjectModal;
