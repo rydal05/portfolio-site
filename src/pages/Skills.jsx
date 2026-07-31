7f310b006e8da089dd98464fd3a6c70a92e46ccd
Ryan Dalton <rdalton2405@gmail.com>
2026-07-31 13:54:18 -0400

    relocate for icons to work when deplopyed

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,42 +1,42 @@
 import ListModal from "../components/ListModal";
 
 const frameworks = [
-	{ name: "React", icon: "../assets/icons/React-Light.svg" },
-	{ name: "Flask", icon: "../assets/icons/Flask-Light.svg" },
-	{ name: "Svelte", icon: "../assets/icons/Svelte.svg" },
-	{ name: "SQLite", icon: "../assets/icons/SQLite.svg" },
-	{ name: "BeautifulSoup", icon: "../assets/icons/Python-Light.svg" },
-	{ name: "TailWindCSS", icon: "../assets/icons/TailwindCSS-Light.svg" },
+	{ name: "React", icon: "../assets/React-Light.svg" },
+	{ name: "Flask", icon: "../assets/Flask-Light.svg" },
+	{ name: "Svelte", icon: "../assets/Svelte.svg" },
+	{ name: "SQLite", icon: "../assets/SQLite.svg" },
+	{ name: "BeautifulSoup", icon: "../assets/Python-Light.svg" },
+	{ name: "TailWindCSS", icon: "../assets/TailwindCSS-Light.svg" },
 ];
 
 const AIML = [
-	{ name: "Hugging Face", icon: "../assets/icons/hf-logo.svg" },
-	{ name: "Ollama", icon: "../assets/icons/ollama.svg" },
-	{ name: "Continue Coding Agent", icon: "../assets/icons/continue.png" },
+	{ name: "Hugging Face", icon: "../assets/hf-logo.svg" },
+	{ name: "Ollama", icon: "../assets/ollama.svg" },
+	{ name: "Continue Coding Agent", icon: "../assets/continue.png" },
 ];
 
 const tools = [
-	{ name: "Docker", icon: "../assets/icons/Docker.svg" },
-	{ name: "Linux", icon: "../assets/icons/Linux-Light.svg" },
-	{ name: "Ghidra", icon: "../assets/icons/Ghidra_logo.svg" },
-	{ name: "Ida", icon: "../assets/icons/IDAIcon.png" },
-	{ name: "Git", icon: "../assets/icons/Git.svg" },
-    { name: "GitHub Actions", icon:"../assets/icons/GithubActions-Light.svg"}
+	{ name: "Docker", icon: "../assets/Docker.svg" },
+	{ name: "Linux", icon: "../assets/Linux-Light.svg" },
+	{ name: "Ghidra", icon: "../assets/Ghidra_logo.svg" },
+	{ name: "Ida", icon: "../assets/IDAIcon.png" },
+	{ name: "Git", icon: "../assets/Git.svg" },
+    { name: "GitHub Actions", icon:"../assets/GithubActions-Light.svg"}
 ];
 
 const languages = [
-	{ name: "C", icon: "../assets/icons/C.svg" },
-	{ name: "C++", icon: "../assets/icons/CPP.svg" },
-	{ name: "Python", icon: "../assets/icons/Python-Light.svg" },
-	{ name: "SQL", icon: "../assets/icons/sql-database-generic-svgrepo-com.svg" },
-	{ name: "Java", icon: "../assets/icons/Java-Light.svg" },
-	{ name: "HTML5", icon: "../assets/icons/HTML.svg" },
-	{ name: "CSS", icon: "../assets/icons/CSS.svg" },
-	{ name: "JavaScript", icon: "../assets/icons/JavaScript.svg" },
-	{ name: "Lua", icon: "../assets/icons/Lua-Light.svg" },
-	{ name: "Delphi", icon: "../assets/icons/delphi-svgrepo-com.svg" },
-	{ name: "GDScript", icon: "../assets/icons/Godot-Light.svg" },
-	{ name: "Rust", icon: "../assets/icons/Rust.svg" },
+	{ name: "C", icon: "../assets/C.svg" },
+	{ name: "C++", icon: "../assets/CPP.svg" },
+	{ name: "Python", icon: "../assets/Python-Light.svg" },
+	{ name: "SQL", icon: "../assets/sql-database-generic-svgrepo-com.svg" },
+	{ name: "Java", icon: "../assets/Java-Light.svg" },
+	{ name: "HTML5", icon: "../assets/HTML.svg" },
+	{ name: "CSS", icon: "../assets/CSS.svg" },
+	{ name: "JavaScript", icon: "../assets/JavaScript.svg" },
+	{ name: "Lua", icon: "../assets/Lua-Light.svg" },
+	{ name: "Delphi", icon: "../assets/delphi-svgrepo-com.svg" },
+	{ name: "GDScript", icon: "../assets/Godot-Light.svg" },
+	{ name: "Rust", icon: "../assets/Rust.svg" },
 ];
 
 function Skills() {
