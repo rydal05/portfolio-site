35128866fb352f6c00c9ec9471edfdd3d611d624
Ryan Dalton <rdalton2405@gmail.com>
2026-07-31 11:56:40 -0400

    about page scaffolding

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,20 +6,28 @@ function About() {
                 <p>Hi, I'm Ryan. I am currently pursuing my undergrad degree at the University of Central Florida for Computer Science.</p>
             </div>
 
+            <p>pic from japan here prolly</p>
+
             <div>
                 <h1>What do I do?</h1>
                 <p>Professionally I enjoy software development, more specifically, I enjoy working with C/C++ and Python to create performant solutions to problems I find intruiging. Solving puzzles is the name of the game baby.</p>
             </div>
+
+            <p>another misc pic here</p>
 
             <div>
                 <h1>What's my stack?</h1>
                 <p>I develop almost exclusively on Linux, rotating between Arch, Fedora, and LMDE (Debian) primarily with C/C++, Python, and whatever wildcard langauge I've determined is required to get a certain job done. As far as webdev is concerned, I've worked with the usual HTML+CSS+JS in addition to frameworks like React and Svelte (with Vite).</p>
             </div>
 
+            <p>more pictures</p>
+
             <div>
                 <h1>What are your dreams? Goals? Aspirations?</h1>
                 <p>The dream since middle school has always been to put into the world a game that I'm proud of. It's not something I pull my full attention to always but it's always something I've carried with me and worked on throughout my life since then. Programming and computer shenanigans aside, I'd love to travel the world. Becoming a polyglot is another one of my long term goals.</p>
             </div>
+
+            
         </>
     )
 }
