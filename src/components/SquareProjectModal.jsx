fcc77db0eb7279ecce9456f8de018e852d03c7bc
Ryan Dalton <rdalton2405@gmail.com>
2026-08-12 12:59:59 -0400

    opening up space for various project modal types

--- src/components/SquareProjectModal.jsx
+++ src/components/SquareProjectModal.jsx
@@ -1,9 +1,9 @@
 import bubble from "./bubble";
 
-function ProjectModal({ link, title, image, text, skills}) {
+function SquareProjectModal({ link, title, image, skills, children}) {
 	return (
 		<>
-			<div className="flex-1 max-w-2xl flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
+			<div className="flex-1 max-w-2xl max-h-2xl flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
 				<h2>
 					<a
 						className="hover:text-[#1d7cff] text-[#e94f37]"
@@ -15,11 +15,11 @@ function ProjectModal({ link, title, image, text, skills}) {
 				{image && ( 
 					<img src={image} className="h-48 w-96 object-contain" />
 				)}
-				<p>{text}</p>
+				<p>{children}</p>
 
 			</div>
 		</>
 	);
 }
 
-export default ProjectModal;
+export default SquareProjectModal;
