b07f31b0a6579c2977a8276b1a1ef92f514a9f85
Ryan Dalton <rdalton2405@gmail.com>
2026-08-12 11:25:17 -0400

    projects are displayed in more nicely laid out blocks

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -3,7 +3,7 @@ import bubble from "./bubble";
 function ProjectModal({ link, title, image, text, skills}) {
 	return (
 		<>
-			<div className="flex-1 flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
+			<div className="flex-1 max-w-2xl flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
 				<h2>
 					<a
 						className="hover:text-[#1d7cff] text-[#e94f37]"
