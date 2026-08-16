af1c17c1ced63ad3ac1fe9974f75676c6d3f6c00
Ryan Dalton <rdalton2405@gmail.com>
2026-08-16 11:44:59 -0400

    remedy weird spacing on portfolio objects

--- src/components/SquareProjectModal.jsx
+++ src/components/SquareProjectModal.jsx
@@ -3,7 +3,7 @@ import bubble from "./bubble";
 function SquareProjectModal({ link, title, image, skills, children}) {
 	return (
 		<>
-			<div className="flex-1 max-w-2xl max-h-2xl flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
+			<div className="flex-1 max-w-2xl max-h-2xl flex-col p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
 				<h2>
 					<a
 						className="hover:text-[#1d7cff] text-[#e94f37]"
--- src/components/TallProjectModal.jsx
+++ src/components/TallProjectModal.jsx
@@ -3,7 +3,7 @@ import bubble from "./bubble";
 function TallProjectModal({ link, title, image, skills, children}) {
 	return (
 		<>
-			<div className="flex max-w-xl min-h-2xl flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
+			<div className="flex max-w-xl min-h-2xl flex-col p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
 				<h2>
 					<a
 						className="hover:text-[#1d7cff] text-[#e94f37]"
