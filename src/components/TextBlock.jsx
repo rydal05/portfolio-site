26bcc10d894427c5fd0eff8c8e484b9d7ac06ae9
Ryan Dalton <rdalton2405@gmail.com>
2026-08-16 22:39:46 -0400

    removed nested <p> in <li>

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -3,7 +3,7 @@ function TextBlock({ title, children }) {
 		<>
 			<div className="flex-1 flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030">
 				<h2 className="p-4">{title}</h2>
-				<p className="text-left p-4">{children}</p>
+				<span className="text-left p-4">{children}</span>
 			</div>
 		</>
 	);
