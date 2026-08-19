0ea9c361133263be0a1f7ce2494af80e6d596fea
Ryan Dalton <rdalton2405@gmail.com>
2026-08-19 15:20:30 -0400

    header is now navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const navigation = [
 export function Header() {
 	return (
 		<>
-			<header className="items-center justify-between pointer-events-auto relative z-50 rounded-full border border-white/24 bg-white/[0.08] transition-all duration-300 hover:bg-white/[0.12] focus-within:bg-white/[0.12] md:block md:hover:backdrop-blur-[2px] md:focus-within:backdrop-blur-[2px] m-4">
+			<header className="sticky top-5 items-center justify-between pointer-events-auto z-50 rounded-full border border-white/24 bg-white/[0.08] transition-all duration-300 hover:bg-white/[0.12] focus-within:bg-white/[0.12] md:block backdrop-blur-[2px] md:focus-within:backdrop-blur-[2px] m-4">
 				<div className="flex justify-between">
 					<div
 						className="font-semibold text-white m-4"
