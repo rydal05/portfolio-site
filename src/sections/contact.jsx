e7ed2ca4eccf8e5d51503833887c65554c994971
Ryan Dalton <rdalton2405@gmail.com>
2026-08-20 16:30:09 -0400

    pretty much all contact info I'd like to give out on my site

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -3,8 +3,33 @@ function Contact() {
 		<>
 			<section id="contact" className="scroll-mt-24">
 				<h1>Extend a hand</h1>
-				<hr />
-				<h1>Come learn about me more personally</h1>
+				<ul className="text-3xl">
+					<li>
+						Email:{" "}
+						<a href="mailto:rdalton2405@gmail.com">
+							rdalton2405@gmail.com
+						</a>
+					</li>
+					<li>
+						Linkedin:{" "}
+						<a href="https://www.linkedin.com/in/rdalton05/">
+							rdalton05
+						</a>
+					</li>
+				</ul>
+
+				<h1>Alternatively, see what I'm up to</h1>
+				<ul className="text-3xl">
+					<li>
+						Github: <a href="https://github.com/rydal05">rydal05</a>
+					</li>
+					<li>
+						Bluesky:{" "}
+						<a href="https://bsky.app/profile/reddolphin.tech">
+							reddolphin.tech
+						</a>
+					</li>
+				</ul>
 			</section>
 		</>
 	);
