5bbe4b839931dd39e18553e0f3dfcd4d623f0ed1
Ryan Dalton <rdalton2405@gmail.com>
2026-08-21 19:39:09 -0400

    remedy last to account for vertical

--- src/components/ListModal.jsx
+++ src/components/ListModal.jsx
@@ -3,7 +3,7 @@ export default function ListModal({ title, text }) {
 		<>
 			<div className="">
 				<h2>{title}</h2>
-				<div className="gap-4 pr-4 overflow-y-auto max-h-[20vw]">
+				<div className="gap-4 pr-4 overflow-y-auto max-h-[20vh]">
 					{text.map((item) => (
 						<div
 							key={item.name}
