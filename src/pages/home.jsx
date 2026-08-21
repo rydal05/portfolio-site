75a98c47b80ff61a77d92bb544534540f322aa41
Ryan Dalton <rdalton2405@gmail.com>
2026-08-21 19:52:51 -0400

    relocation of quotes into proper components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,57 +10,78 @@ import Portfolio from "/src/sections/prof/portfolio";
 import Skills from "/src/sections/prof/skills";
 import Contact from "/src/sections/prof/contact";
 import Deployed from "/src/sections/prof/deployed";
+import Quote from "/src/components/Quote";
 
 function Home() {
 	return (
 		<>
 			<About />
-			<h4>
-				Alas, <i>Ryan</i> is going . . . Undesired, unwanted them, What
-				makes them go? It is nothing else than the principle of the man
-				who has the reason for being.
-				<br/>
-				嗚呼、斑鳩が行く・・・・・・
-				望まれることなく、浮き世から捨てられし彼等を動かすもの。
-				それは、生きる意志を持つ者の意地に他ならない。
-			</h4>
+			<Quote>
+				<ul className="flex justify-between">
+					<li>
+						<ul>
+							<li>
+								Alas, <i>Ryan</i> is going . . . Undesired,
+								unwanted
+							</li>
+							<li>
+								them, What makes them go? It is nothing else
+								than the
+							</li>
+							<li>
+								principle of the man who has the reason for
+								being.
+							</li>
+						</ul>
+					</li>
+					<li>
+						<ul>
+							<li>嗚呼、斑鳩が行く・・・・・・</li>
+							<li>
+								望まれることなく、浮き世から捨てられし彼等を動かすもの。
+							</li>
+							<li>
+								それは、生きる意志を持つ者の意地に他ならない。
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</Quote>
 			<hr className="m-20" />
 
 			<Education />
-			<h4>
+			<Quote>
 				I will not die until I achieve something. Even though the ideal
 				is high, I never give in. Therefore, I never die with regrets.
-				<br />
-				
-			</h4>
+			</Quote>
 			<hr className="m-20" />
 
 			<Portfolio />
-			<h4>
+			<Quote>
 				What everyone wants is but a helping hand. Just enough to save
 				them from the abyss.
-			</h4>
+			</Quote>
+			<hr className="m-20" />
+
+			<Deployed />
+			<Quote></Quote>
 			<hr className="m-20" />
 
 			<Skills />
-			<h4>
+			<Quote>
 				Even if it seems certain that you will lose, retaliate. Neither
 				wisdom nor technique has a place in this. A real man does not
 				think of victory or defeat. He plunges recklessly towards an
 				irrational death. By doing this, you will awaken from your
 				dreams.
-			</h4>
+			</Quote>
 			<hr className="m-20" />
 
 			<Contact />
-			<h4>
+			<Quote>
 				Respect, Honesty, Courage, Rectitude, Loyalty, Honour,
 				Benevolence
-			</h4>
-			<hr className="m-20" />
-
-			<Deployed />
-			<h4>hello</h4>
+			</Quote>
 			<hr className="m-20" />
 		</>
 	);
