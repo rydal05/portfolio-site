82bc3a8c85dfe2b436e9d2c020ab54bc82520931
Ryan Dalton <rdalton2405@gmail.com>
2026-08-21 20:17:54 -0400

    centered video section descriptions

--- src/sections/prof/deployed.jsx
+++ src/sections/prof/deployed.jsx
@@ -3,16 +3,18 @@ import InfiniteVideoCarousel from "/src/components/InfiniteVideoCarousel";
 export default function Deployed() {
 	return (
 		<>
-			<h1>Freelance Work</h1>
-			<h2>— 3d Animation & Programming Work I've Submitted —</h2>
+			<h1>— Freelance Work —</h1>
+			<h2>3d animation & programming work I've submitted</h2>
 			<InfiniteVideoCarousel />
-			<p className="max-w-[50%]">
-				Assisted in driving ~5 million in interactions (Likes, Comments,
-				Reposts) across YouTube and Tiktok through 3d animation and
-				stage programming work. Additionally drove ~75 million views on
-				YouTube alone, with an additional ~50 million on the TikTok
-				platform.
-			</p>
+			<div className="flex flex-col items-center">
+				<p className="w-[50%] mx-auto">
+					Assisted in driving ~5 million in interactions (Likes,
+					Comments, Reposts) across YouTube and Tiktok through 3d
+					animation and stage programming work. Additionally drove ~75
+					million views on YouTube alone, with an additional ~50
+					million on the TikTok platform.
+				</p>
+			</div>
 		</>
 	);
 }
