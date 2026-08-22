eca468739ffa7f985691d1a717f9ae12ea84bb92
Ryan Dalton <rdalton2405@gmail.com>
2026-08-21 20:06:59 -0400

    scaffolding personal section

--- src/sections/prof/personalLink.jsx
+++ src/sections/prof/personalLink.jsx
@@ -2,7 +2,8 @@ export default function Personal() {
 	return (
 		<>
 			<section id="personal">
-                If you'd like to know more about me and my personal life, click here.
+				<h1>Get to know me better</h1>
+                <p>If you'd like to know more about me and my personal life, click here.</p>
             </section>
 		</>
 	);
