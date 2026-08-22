55b77dfab8ff8ab065bcc99f372dec650810424b
Ryan Dalton <rdalton2405@gmail.com>
2026-08-21 20:05:26 -0400

    update contact to split for contact & see me sections

--- src/sections/prof/contact.jsx
+++ src/sections/prof/contact.jsx
@@ -1,37 +1,41 @@
 export default function Contact() {
 	return (
 		<>
-			<section id="contact" className="scroll-mt-24">
-				<h1>Extend a hand</h1>
-				<ul className="text-3xl">
-					<li>
-						Email:{" "}
-						<a href="mailto:rdalton2405@gmail.com">
-							rdalton2405@gmail.com
-						</a>
-					</li>
-					<li>
-						Linkedin:{" "}
-						<a href="https://www.linkedin.com/in/rdalton05/">
-							rdalton05
-						</a>
-					</li>
-				</ul>
+			<section id="contact" className="scroll-mt-24 flex justify-between">
+				<div>
+					<h1>Extend a hand</h1>
+					<ul className="text-3xl">
+						<li>
+							Email:{" "}
+							<a href="mailto:rdalton2405@gmail.com">
+								rdalton2405@gmail.com
+							</a>
+						</li>
+						<li>
+							Linkedin:{" "}
+							<a href="https://www.linkedin.com/in/rdalton05/">
+								rdalton05
+							</a>
+						</li>
+					</ul>
+				</div>
 
-				<h1>Alternatively, see what I'm up to</h1>
-				<ul className="text-3xl">
-					<li>
-						Github: <a href="https://github.com/rydal05">rydal05</a>
-					</li>
-					<li>
-						Bluesky:{" "}
-						<a href="https://bsky.app/profile/reddolphin.tech">
-							reddolphin.tech
-						</a>
-					</li>
-				</ul>
+				<div>
+					<h1>Alternatively, see what I'm up to</h1>
+					<ul className="text-3xl">
+						<li>
+							Github:{" "}
+							<a href="https://github.com/rydal05">rydal05</a>
+						</li>
+						<li>
+							Bluesky:{" "}
+							<a href="https://bsky.app/profile/reddolphin.tech">
+								reddolphin.tech
+							</a>
+						</li>
+					</ul>
+				</div>
 			</section>
 		</>
 	);
 }
-
