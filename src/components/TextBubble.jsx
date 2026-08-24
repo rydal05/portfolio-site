7d4bc7d64b989d7a9a6e1d6c8d8ffb1952c2fd35
Ryan Dalton <rdalton2405@gmail.com>
2026-08-24 13:31:19 -0400

    minor semantic changes

--- src/components/TextBubble.jsx
+++ src/components/TextBubble.jsx
@@ -1,7 +1,7 @@
 function TextBubble({ title, text }) {
 	return (
 		<>
-			<div className="items-center justify-between pointer-events-auto relative z-50 rounded-xl border border-white/24 bg-white/[0.08] transition-all duration-300 hover:bg-white/[0.12] focus-within:bg-white/[0.12] md:block md:hover:backdrop-blur-[2px] md:focus-within:backdrop-blur-[2px] m-4">
+			<div className="items-center justify-between pointer-events-auto relative z-50 rounded-xl border border-white/24 bg-white/8 transition-all duration-300 hover:bg-white/12 focus-within:bg-white/12 md:block md:hover:backdrop-blur-[2px] md:focus-within:backdrop-blur-[2px] m-4">
 				<h2 className="p-4">{title}</h2>
 				<p className="text-left p-4">{text}</p>
 			</div>
