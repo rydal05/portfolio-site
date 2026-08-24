de3ca8ff0a2ad7505565a7cbf58012cbbb2f835c
Ryan Dalton <rdalton2405@gmail.com>
2026-08-24 10:18:42 -0400

    z-index management + expandability through hover

--- src/components/SquareProjectModal.jsx
+++ src/components/SquareProjectModal.jsx
@@ -3,7 +3,7 @@ import bubble from "/src/components/bubble";
 export default function SquareProjectModal({ link, title, image, languages, frameworks, tools, libraries, children }) {
 	return (
 		<>
-			<div className="flex-1 max-w-2xl max-h-2xl flex-col p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
+			<div className="flex-1 max-w-2xl max-h-2xl flex-col p-10 gap-5 bg-[#212121] hover:bg-[#303030] min-expandable">
 				<h2>
 					<a
 						className="hover:text-[#1d7cff] text-[#e94f37]"
--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -1,7 +1,7 @@
 export default function TextBlock({ title, children }) {
 	return (
 		<>
-			<div className="flex-1 flex flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030]">
+			<div className="flex-1 flex flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030] min-expandable z-auto">
 				<div className="min-h-[4rem] flex items-end w-full">
 					<h2 className="text-xl font-bold text-left">{title}</h2>
 				</div>
