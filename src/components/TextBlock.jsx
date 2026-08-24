3fa37216c9e1ec99ee956d5bbc3a46c3e7ac8a3e
Ryan Dalton <rdalton2405@gmail.com>
2026-08-24 10:21:51 -0400

    tiny semantic garbage fix

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -2,7 +2,7 @@ export default function TextBlock({ title, children }) {
 	return (
 		<>
 			<div className="flex-1 flex flex-col items-start p-10 gap-5 bg-[#212121] hover:bg-[#303030] min-expandable z-auto">
-				<div className="min-h-[4rem] flex items-end w-full">
+				<div className="min-h-16 flex items-end w-full">
 					<h2 className="text-xl font-bold text-left">{title}</h2>
 				</div>
 				<div className="w-full text-left">{children}</div>
