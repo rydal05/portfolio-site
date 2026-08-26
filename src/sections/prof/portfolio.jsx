9f6b7b1bd23d2f7ba0c47cd7f680cdf9007f9dd5
Ryan Dalton <rdalton2405@gmail.com>
2026-08-26 10:32:51 -0400

    center portfolio elements

--- src/sections/prof/portfolio.jsx
+++ src/sections/prof/portfolio.jsx
@@ -8,7 +8,7 @@ export default function Portfolio() {
 			<section id="portfolio" className="scroll-mt-30">
 				<h1>— Collected Portfolio Works —</h1>
 				{/* as you scroll down these should pop into view */}
-				<div className="flex flex-col justify-between">
+				<div className="flex flex-col justify-between items-center">
 					<div className="flex flex-row justify-between">
 						<SquareProjectModal
 							link="https://github.com/rydal05/SuruScraper"
